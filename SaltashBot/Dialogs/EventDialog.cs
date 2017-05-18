b9f87288a713e9f95adf631ea13837237748735c
Ross Smith <rosmith@microsoft.com>
2017-05-18 12:36:18 +0100

    Initial commit of Bot

---
+++ SaltashBot/Dialogs/EventDialog.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SaltashBot.Dialogs
+{
+    [Serializable]
+    public class EventDialog : IDialog<object>
+    {
+        private int attempts = 3;
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            await context.PostAsync("Hi, I'm the Saltash event bot. Are you looking for events?");
+
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var message = await result;
+
+            /* If the message returned is a valid name, return it to the calling dialog. */
+            if ((message.Text != null) && (message.Text.Trim().Length > 0))
+            {
+                /* Completes the dialog, removes it from the dialog stack, and returns the result to the parent/calling
+                    dialog. */
+                var url = "https://www.saltash.website/api/apiaicalendar";
+                var httpClient = new HttpClient();
+                HttpResponseMessage response = await httpClient.PostAsync(url, null);
+
+                Mausty mausty = await response.Content.ReadAsAsync<Mausty>();
+                //var message = await argument;
+                await context.PostAsync("I found: " + mausty.displayText);
+                context.Done(mausty.displayText);
+            }
+            /* Else, try again by re-prompting the user. */
+            else
+            {
+                --attempts;
+                if (attempts > 0)
+                {
+                    await context.PostAsync("I'm sorry, I don't understand your reply. Are you looking for events?");
+                    context.Wait(this.MessageReceivedAsync);
+                }
+                else
+                {
+                    /* Fails the current dialog, removes it from the dialog stack, and returns the exception to the 
+                        parent/calling dialog. */
+                    context.Fail(new TooManyAttemptsException("Message was not a string or was an empty string."));
+                }
+            }
+        }
+    }
+
+    public class Mausty
+    {
+        public string speech;
+
+        public string displayText;
+
+        public string source;
+    }
+}
---
+++ SaltashBot/Dialogs/RootDialog.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System.Threading;
+
+namespace SaltashBot.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            // Root dialog initiates and waits for the next message from the user. 
+            // When a message arrives, call MessageReceivedAsync.
+            context.Wait(this.MessageReceivedAsync);
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            /* When MessageReceivedAsync is called, it's passed an IAwaitable<IMessageActivity>. To get the message,
+             *  await the result. */
+            var message = await result;
+
+            await this.SendWelcomeMessageAsync(context);
+        }
+
+        private async Task SendWelcomeMessageAsync(IDialogContext context)
+        {
+//            await context.PostAsync("Hi, I'm the Saltash bot. Let's get started.");
+
+            context.Call(new EventDialog(), this.ResumeAfterEventDialog);
+        }
+
+        public async Task ResumeAfterEventDialog(IDialogContext context, IAwaitable<object> result)
+        {
+            // Store the value that EventDialog returned. 
+            // (At this point, new order dialog has finished and returned some value to use within the root dialog.)
+ //           var resultFromNewOrder = await result;
+
+//            await context.PostAsync($"Thanks for using the Saltash bot: {resultFromNewOrder}");
+
+            await this.SendWelcomeMessageAsync(context);
+        }
+
+        //public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        //{
+        //    var message = await result; // We've got a message!
+        //    if (message.Text.ToLower().Contains("event"))
+        //    {
+        //        // User said 'event', so invoke the Event Search Dialog and wait for it to finish.
+        //        // Then, call ResumeAfterEventSearchDialog.
+        //        await context.Forward(new EventDialog(), ResumeAfterEventDialog, message, CancellationToken.None);
+        //    }
+        //    else
+        //    {
+        //        // calculate something for us to return
+        //        int length = (message.Text ?? string.Empty).Length;
+
+        //        // return our reply to the user
+        //        await context.PostAsync($"How can I help you?");
+        //    }
+        //    context.Wait(MessageReceivedAsync);
+        //}
+    }
+}
